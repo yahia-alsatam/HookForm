4df5e83997afa7edfffa05be358d8568a8b7b18b
yahia-alsatam <yahiaalsatam2024@gmail.com>
2025-02-06 13:31:31 +0300

    Form add input password

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import "./App.css";
 import FormHook from "./Form/FormHook";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
--- src/Form/FormHook.jsx
+++ src/Form/FormHook.jsx
@@ -1,37 +1,54 @@
+import "./FormHook.css";
+
 const FormHook = () => {
   return (
     <>
       <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
+        <div class="form-group mb-3">
+          <label className="form-label" for="name">
+            name
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="name"
+            placeholder="Enter name"
+          />
+        </div>
+        <div class="form-group mb-3">
+          <label className="form-label" for="email">
+            email
+          </label>
           <input
             type="email"
             class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
+            id="email"
             placeholder="Enter email"
           />
-
-          <small id="emailHelp" class="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
+        <div class="form-group mb-3">
+          <label className="form-label" for="password">
+            password
+          </label>
           <input
             type="password"
             class="form-control"
-            id="exampleInputPassword1"
-            placeholder="Password"
+            id="password"
+            placeholder="Enter password"
           />
         </div>
-        <div class="form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-
-          <label class="form-check-label" for="exampleCheck1">
-            Check me out
+        <div class="form-group mb-3">
+          <label className="form-label" for="confirmPassword">
+            confirmPassword
           </label>
+          <input
+            type="confirmPassword"
+            class="form-control"
+            id="confirmPassword"
+            placeholder="Enter email"
+          />
         </div>
+
         <button type="submit" class="btn btn-primary">
           Submit
         </button>
